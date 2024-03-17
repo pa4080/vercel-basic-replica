7eb9d78f2592e973bd1d72c54667bc1ce86a2181
pa4080 <spas.z.spasov@metalevel.tech>
2024-03-17 14:51:10 +0200

    chore: add method to Zod

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,46 @@
 /**
  * The ENV_VARS are managed via Doppler at https://dashboard.doppler.com/
+ * https://github.com/colinhacks/zod/issues/63
  */
-import { z } from "zod";
+import { z as zod } from "zod";
+
+const z = zod as typeof zod & { stringRequired: (key?: string) => zod.ZodString };
+
+z.stringRequired = (key: string | undefined = undefined) =>
+	zod
+		.string()
+		.trim()
+		.min(1, { message: `${key ? `${key} is ` : ""}Required!` });
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(["development", "test", "production"]),
-	UPLOAD_SERVICE_PORT: z.string().trim().min(1, { message: "UPLOAD_SERVICE_PORT is required!" }),
+	NODE_ENV: z.enum(["dev", "stg", "prod"]),
+	UPLOAD_SERVICE_PORT: z.stringRequired(),
+	CLOUDFLARE_ACCOUNT_ID: z.stringRequired(),
+	CLOUDFLARE_API_TOKEN: z.stringRequired(),
+	CLOUDFLARE_API_ACCESS_KEY_ID: z.stringRequired(),
+	CLOUDFLARE_API_ACCESS_KEY_SECRET: z.stringRequired(),
+	CLOUDFLARE_API_ENDPOINT: z.stringRequired(),
 });
 
-const { NODE_ENV, UPLOAD_SERVICE_PORT } = process.env;
+const {
+	NODE_ENV,
+	UPLOAD_SERVICE_PORT,
+	CLOUDFLARE_ACCOUNT_ID,
+	CLOUDFLARE_API_TOKEN,
+	CLOUDFLARE_API_ACCESS_KEY_ID,
+	CLOUDFLARE_API_ACCESS_KEY_SECRET,
+	CLOUDFLARE_API_ENDPOINT,
+} = process.env;
 
-const parsedResults = envSchema.safeParse({ NODE_ENV, UPLOAD_SERVICE_PORT });
+const parsedResults = envSchema.safeParse({
+	NODE_ENV,
+	UPLOAD_SERVICE_PORT,
+	CLOUDFLARE_ACCOUNT_ID,
+	CLOUDFLARE_API_TOKEN,
+	CLOUDFLARE_API_ACCESS_KEY_ID,
+	CLOUDFLARE_API_ACCESS_KEY_SECRET,
+	CLOUDFLARE_API_ENDPOINT,
+});
 
 if (!parsedResults.success) {
 	console.error(parsedResults.error);
@@ -22,6 +52,6 @@ export const env = parsedResults.data;
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace NodeJS {
-		interface ProcessEnv extends z.infer<typeof envSchema> {}
+		interface ProcessEnv extends zod.infer<typeof envSchema> {}
 	}
 }
