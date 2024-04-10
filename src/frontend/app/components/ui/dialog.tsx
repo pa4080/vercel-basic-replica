8044b1f90c9f4283a619da88e062b08b5e8c17f7
pa4080 <spas.z.spasov@metalevel.tech>
2024-04-10 14:57:55 +0300

    refactor: move frontend into src

--- src/frontend/app/components/ui/dialog.tsx
+++ src/frontend/app/components/ui/dialog.tsx
@@ -27,6 +27,7 @@ const DialogOverlay = React.forwardRef<
 		{...props}
 	/>
 ));
+
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogCloseWrapper: React.FC = () => (
@@ -65,11 +66,13 @@ const DialogContent = React.forwardRef<
 		</DialogPrimitive.Content>
 	</DialogPortal>
 ));
+
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
 	<div className={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)} {...props} />
 );
+
 DialogHeader.displayName = "DialogHeader";
 
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
@@ -78,6 +81,7 @@ const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivEleme
 		{...props}
 	/>
 );
+
 DialogFooter.displayName = "DialogFooter";
 
 const DialogTitle = React.forwardRef<
@@ -90,6 +94,7 @@ const DialogTitle = React.forwardRef<
 		{...props}
 	/>
 ));
+
 DialogTitle.displayName = DialogPrimitive.Title.displayName;
 
 const DialogDescription = React.forwardRef<
@@ -102,6 +107,7 @@ const DialogDescription = React.forwardRef<
 		{...props}
 	/>
 ));
+
 DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
 export {
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,17 +1,19 @@
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+
 import { defineConfig } from "vite";
+
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
+			"@": path.resolve(__dirname, "./app"),
 		},
 	},
 	// https://stackoverflow.com/questions/66863200/changing-the-input-and-output-directory-in-vite
 	build: {
-		outDir: "../dist/frontend",
+		outDir: "../../dist/frontend",
 	},
 });
