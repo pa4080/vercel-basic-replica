b720d589617bf1dfed2b7f62b036a9f799c4dff7
pa4080 <spas.z.spasov@metalevel.tech>
2024-05-12 11:06:21 +0300

    chore: rollback a change

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,1 @@
-module.exports = {
-	extends: [
-		"@commitlint/config-conventional",
-		// "@commitlint/config-conventional"
-	],
-};
+module.exports = { extends: ["@commitlint/config-conventional"] };
